4851636cd7934816761055e182fe88b850c7c556
manhhaa <hanguyenns793@gmail.com>
2022-12-23 09:21:31 +0700

    fix

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -18,25 +18,25 @@ export class HomeComponent implements OnInit {
     {
       id: 1,
       productContent: "Casio CTKVK3 PAK 61-Key Premium Keyboard Bundle",
-      price: "$165.00",
+      price: 165.00,
       img: "/assets/img/keyboard1.jpg",
     },
     {
       id: 2,
       productContent: "Yamaha DGX650B Digital Piano",
-      price: "$121.00",
+      price: 121.00,
       img: "/assets/img/keyboard2.jpg",
     },
     {
       id: 3,
       productContent: "Hamzer 61 Key Electronic Music Piano Keyboard",
-      price: "$165.00",
+      price: 165.00,
       img: "/assets/img/keyboard3.jpg",
     },
     {
       id: 4,
       productContent: "Hamzer 61 Key Electric Music Keyboard Piano with Stand",
-      price: "$143.00",
+      price: 143.00,
       img: "/assets/img/keyboard4.jpg",
     },
   ];
